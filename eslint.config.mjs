12359c933aacf72c10a7610c31f705f6a970d530
swordofastrea <yiwu0819@gmail.com>
2025-07-13 20:37:30 -0700

    build: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,40 +1,47 @@
-import tsParser from "@typescript-eslint/parser";
+import style from "@isentinel/eslint-config";
 
-export default [
-  {
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: "module",
-        jsx: true,
-        project: "./tsconfig.json",
-      },
-    },
-    ignores: ["/out"],
-  },
-  {
-    rules: {
-      "antfu/consistent-list-newline": "off",
-      curly: "off",
-      "max-lines-per-function": "off",
-      "roblox/lua-truthiness": "off",
-      "ts/no-confusing-void-expression": "off",
-      "ts/no-non-null-assertion": "off",
-      "ts/no-redundant-type-constituents": "off",
-      "ts/no-unnecessary-type-assertion": "off",
-      "ts/no-unsafe-argument": "off",
-      "ts/no-unsafe-assignment": "off",
-      "ts/no-unsafe-call": "off",
-      "ts/no-unsafe-member-access": "off",
-      "ts/no-unsafe-return": "off",
-      "ts/no-unused-expressions": "off",
-      "ts/only-throw-errors": "off",
-      "ts/strict-boolean-expressions": "off",
-      "unicorn/prefer-logical-operator-over-ternary": "off",
-      "react/no-create-ref": "off",
-      "ts/no-misused-promises": "off",
-      "test/valid-expect": "off",
-    },
-  },
-];
+export default style({
+	gitignore: true,
+	ignores: ["node_modules", "places/*/out", "places/*/dist"],
+	jsdoc: false,
+	jsx: true,
+	languageOptions: {
+		parserOptions: {
+			project: ["./tsconfig.json", "./places/*/tsconfig.json", "tsconfig.build.json"],
+			tsconfigRootDir: import.meta.dirname,
+		},
+	},
+	markdown: false,
+	react: true,
+	roblox: true,
+	rules: {
+		"antfu/consistent-list-newline": "off",
+		"curly": "off",
+		"max-lines-per-function": "off",
+		"react/no-create-ref": "off",
+		"roblox/lua-truthiness": "off",
+		"shopify/prefer-module-scope-constants": "off",
+		"style/jsx-sort-props": "off",
+		"test/valid-expect": "off",
+		"ts/no-confusing-void-expression": "off",
+		"ts/no-misused-promises": "off",
+		"ts/no-non-null-assertion": "off",
+		"ts/no-redundant-type-constituents": "off",
+		"ts/no-unnecessary-type-assertion": "off",
+		"ts/no-unsafe-argument": "off",
+		"ts/no-unsafe-assignment": "off",
+		"ts/no-unsafe-call": "off",
+		"ts/no-unsafe-member-access": "off",
+		"ts/no-unsafe-return": "off",
+		"ts/no-unused-expressions": "off",
+		"ts/only-throw-error": "off",
+		"ts/strict-boolean-expressions": "off",
+		"unicorn/prefer-logical-operator-over-ternary": "off",
+	},
+	spellCheck: true,
+	test: true,
+	type: "game",
+	typescript: {
+		tsconfigPath: "tsconfig.json",
+	},
+});
